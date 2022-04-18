173ffa9884c6eee8be44e78a559a51869fb790a6
Martynov Evgeny <martynov@wimix.by>
2022-04-18 19:33:56 +0300

    HW

---
+++ NetworkApp/Controllers/Detail/DetailViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DetailViewController.swift
+//  NetworkApp
+//
+//  Created by Martynov Evgeny on 18.04.22.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+    
+    @IBOutlet private weak var nameLbl: UILabel!
+    @IBOutlet private weak var usernameLbl: UILabel!
+    @IBOutlet private weak var emailLbl: UILabel!
+    @IBOutlet private weak var phoneLbl: UILabel!
+    @IBOutlet private weak var websiteLbl: UILabel!
+    @IBOutlet private weak var streetLbl: UILabel!
+    @IBOutlet private weak var suitLbl: UILabel!
+    @IBOutlet private weak var cityLbl: UILabel!
+    @IBOutlet private weak var zipCodeLbl: UILabel!
+
+    @IBOutlet private weak var companyLbl: UILabel!
+    @IBOutlet private weak var bsLbl: UILabel!
+    @IBOutlet private weak var chLbl: UILabel!
+
+    var user: User?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        guard let name = user?.name else { return }
+        nameLbl.text = name
+        usernameLbl.text = user?.username ?? "No data"
+        emailLbl.text = user?.email
+        phoneLbl.text = user?.phone
+        websiteLbl.text = user?.website
+        streetLbl.text = user?.address?.street
+        suitLbl.text = user?.address?.suite
+        cityLbl.text = user?.address?.city
+        zipCodeLbl.text = user?.address?.zipcode
+        companyLbl.text = user?.company?.name
+        bsLbl.text = user?.company?.bs
+        chLbl.text = user?.company?.catchPhrase
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "showPosts",
+//           let postsVC = segue.destination as? PostsTableViewController
+//        {
+//            postsVC.user = user
+//        } else if segue.identifier == "albomsSegue",
+//                  let albomsTableVC = segue.destination as? AlbomsTableVC
+//        {
+//            albomsTableVC.user = user
+//        }
+    }
+}
--- NetworkApp/Controllers/Users/UsersTVC.swift
+++ NetworkApp/Controllers/Users/UsersTVC.swift
@@ -30,51 +30,23 @@ class UsersTVC: UITableViewController {
         cell.setupUI(user: users[indexPath.row])
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    // MARK: - TableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let user = users[indexPath.row]
+        performSegue(withIdentifier: "showDetail", sender: user)
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let desination = segue.destination as? DetailViewController,
+           let user = sender as? User
+        {
+            desination.user = user
+        }
     }
-    */
 }
 
 extension UsersTVC: UsersTVCProtocol {
